0ce1a8d455fc1d3980f9ebb085584b68742200af
zLhYd <adigans221@gmail.com>
2022-10-29 16:26:27 +0700

    something

--- src/components/navbar/Nav.jsx
+++ src/components/navbar/Nav.jsx
@@ -56,7 +56,7 @@ const Nav = () => {
         <div className="collapse navbar-collapse" id="navbarNav">
           <ul className="navbar-nav fw-bold">
             <li className="nav-item ">
-              <a className="navLink nav-link" href="/dee">
+              <a className="navLink nav-link" href="/">
                 Home
               </a>
             </li>
